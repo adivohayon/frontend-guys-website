365ae87ece90248a33e61855f14b77eb7018c753
Adiv Ohayon <adivohayon@gmail.com>
2017-04-25 14:11:09 +0300

    setup dev script

--- config/setup_dev.sh
+++ config/setup_dev.sh
@@ -2,13 +2,23 @@
 
 set -o errexit # Exit on error
 
-echo 'Creating Dev-Machine...';
-docker-machine create --driver virtualbox dev-machine
+# DEVMACHINE = "$(docker-machine ls -q | grep '^dev-machine$')";
+# echo ${DEVMACHINE};
+
+MACHINE="$(docker-machine ls -q | grep '^dev-machine$')"
+if [ -z "$MACHINE" ]
+then
+	echo 'Dev-Machine does not exist: Creating Dev-Machine...';
+    docker-machine create --driver virtualbox dev-machine 
+fi
+
 
 echo "Setting dev-machine in docker env";
 eval "$(docker-machine env dev-machine)"
 
-echo 'Installing docker-osx-dev...'
+echo 'Installing docker-osx-dev...';
+curl -o /usr/local/bin/docker-osx-dev https://raw.githubusercontent.com/brikis98/docker-osx-dev/master/src/docker-osx-dev
+chmod +x /usr/local/bin/docker-osx-dev
 docker-osx-dev install
 
 echo 'Installing angular CLI';
