9a37de32622439f3dbdfd6bb739a82494e0ae501
Ben Haddad <benisdman@gmail.com>
2017-05-24 13:12:56 +0300

    [section][design_and_development]

--- wordpress/wp-content/themes/fe-guys/template-parts/front-page/design-and-development.php
+++ wordpress/wp-content/themes/fe-guys/template-parts/front-page/design-and-development.php
@@ -3,6 +3,41 @@
 ==============================================-->
 <div class="row section" id="section-design-and-development">
 	<div class="col-xs-12">
-		<h2>Design and Development</h2>
+		<h2>Design &amp; <span class="green">Development</span></h2>
+		<h3>We take your idea through a unique process that yeilds a beautiful, modular and stable product in less time, with UI/UX best-practices.</h3>
+		<div class="eggs-container">
+			<hr>
+			<ul>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Idea</div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">UX design</div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Component-Driven<br>UI Design</div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Styleguide </div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Dev Environment</div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Components<br>Development</div>
+				</li>
+				<li>
+					<div class="egg"></div>
+					<div class="egg-text">Product</div>
+				</li>
+			</ul>
+		</div>
+		<h4>Our deliverables are a stable product, a styleguide and development environment for future cycles.</h4>
 	</div>
 </div><!-- #section-design-and-development -->
